18e0c340e2e1dbf4d2a4ff17db5a18687550718a
Jonathan Vegas <jonathanvegas@Jonathans-MacBook-Air.local>
2022-07-26 13:14:44 -0400

    New Updates

--- arrays.js
+++ arrays.js
@@ -33,8 +33,39 @@ let teachers = [
 // const bobby = studentNames.find((student) => student === "bobby")
 // console.log(bobby)
 
-const somebobby = studentNames.some((student) => student === "bobby")
-console.log("Do we have at least a bobby?", somebobby)
+//some()
+// const somebobby = studentNames.some((student) => student === "bobby")
+// console.log("Do we have at least a bobby?", somebobby)
+// //every()
+// const everybobby = studentNames.every((student) => student === "bobby")
+// console.log("Everyone's a bobbys", everybobby)
 
-const everybobby = studentNames.every((student) => student === "bobby")
-console.log("Everyone's a bobbys", everybobby)
+//forEach()
+// studentNames.forEach((student) => {
+// 	console.log(student)
+// })
+
+//map()
+// function giveMeTheName(object){
+// 	return `${object.name} is a teacher` 
+// }
+// const justTheNames = teachers.map(giveMeTheName)
+// // const justTheNames = teachers.map((teacher) => {
+// // 	return teacher.name
+// // })
+// console.log(justTheNames)
+
+//.reduce()
+let numbers = [10,20,30]
+
+let totals = numbers.reduce((accumulator, currentValue , index, array) => {
+	return accumulator + currentValue
+},0) //0 is total starting point
+
+let total = 0
+for(let i = 0; i < numbers.length; i++){
+	total += numbers[i]
+}
+total = numbers.reduce((total,currentNumber) => total + currentNumber,0)
+
+console.log(total)
